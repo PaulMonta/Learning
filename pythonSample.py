d376133978a096ff4f87da9731ff7cc0fdb54789
Paul MONTA <paul.monta@etu.u-paris.fr>
2024-05-28 22:54:26 +0200

    Ajout du fichier index.html

---
+++ pythonSample.py
@@ -0,0 +1,10 @@
+print("--------------------")
+
+print("--------------------")
+print("test ")
+# i want a function that takes in a list of numbers and returns the sum of the numbers in the list
+def sumList(list):
+    sum = 0
+    for i in list:
+        sum += i
+    return sum
